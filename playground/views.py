530cf6297a8d79cc15326a603b9d3e26335f6501
mayanja <mayanjer3@gmail.com>
2026-02-16 16:56:28 +0300

    commit message

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product
+from store.models import Product, Customer
 
 def say_hello(request):
-    query_set = Product.objects.filter(unit_price__range = (20, 50) )
-
-    print(query_set)
+    query_set = Customer.objects.filter(email__endswith = '.com')
 
 
     return render(request, 'hello.html', {'name': 'Mayanja', 'products': list(query_set)})
